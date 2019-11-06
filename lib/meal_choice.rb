096805236a88aaacafd0c61c79485283af0f3dfe
Abzal Bacchus <bacchusabzal1@gmail.com>
2019-11-06 09:32:50 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,16 @@
 # Your code here
+
+
+
+# The hosts are going the meal information to keep track of what everyone is
+# eating, so make sure the return value includes the **three** food items you've
+# chosen by returning: "A plate of #{protein} with #{veg1} and #{veg2}."
+
+# **Note:** An _explicit_ or _implicit_ return can be used.
+
+def meal_choice(veg1, veg2, protein = "meat")
+  puts "What a nutritious meal!"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  return "A plate of #{protein} with #{veg1} and #{veg2}."
+end 
+meal_choice("brocolli", "carrot")
